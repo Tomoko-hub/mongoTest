25006c062cf308d687b0427f497d00ad64b51584
TomokoUehara <71818558+Tomoko-hub@users.noreply.github.com>
2023-04-20 21:23:57 +0300

    Update database.js
    delete database

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async()=>{
     try{
-        await mongoose.connect("mongodb+srv://mongoTest:mongoTest@cluster0.xbhpgoh.mongodb.net/appDataBase?retryWrites=true&w=majority")
+        await mongoose.connect("")
         .then(()=>console.log("Mongo db connected"));
     }catch(err){
         console.log("Mongo db connection failed")
